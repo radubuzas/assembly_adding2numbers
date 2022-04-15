28cd6858ec0e3841b28e8ffbb8e7db36177ffb08
radubuzas <92592325+radubuzas@users.noreply.github.com>
2022-04-15 18:43:23 +0300

    Update adding2numbers.asm
    Added comments

--- adding2numbers.asm
+++ adding2numbers.asm
@@ -13,49 +13,51 @@ section .text
 	global _start
 _start:
 	
-	xor eax, eax
-	mov [x], eax
-	mov [cnt], eax
-	mov [ok], eax
+	xor eax, eax	; eax = 0
 	
-	mov eax, 10
-	mov [p], eax
+	mov [x], eax	; x = eax = 0
+	mov [cnt], eax	; cnt = eax = 0
+	mov [ok], eax	; ok = eax = 0
 	
-	mov eax, 0x30
-	mov [zero], eax
+	mov eax, 10	; eax = 10
+	mov [p], eax	; p = 10
 	
-	;citire numar x
+	mov eax, 0x30	; eax = ascii code of '0' - 48 - 0x30
+	mov [zero], eax	; zero = ascii code of '0' - 48 - 0x30
+	
+	;reading x
 	et_citire:
-	    xor edx, edx
-	    mov [x_buffer], edx
+	    xor edx, edx	; edx = 0
+	    mov [x_buffer], edx	; x_buffer = 0
 	    
-    	mov eax, 0x3
-    	mov ebx, 0x0
-    	mov ecx, x_buffer
-    	mov edx, 0x1
-    	int 0x80
+    	mov eax, 0x3		; eax = 3 - code for reading
+    	xor ebx, ebx		; ebx = 0 - iostream
+    	mov ecx, x_buffer	; ecx = &x_buffer (the address of x_buffer)
+    	mov edx, 0x1		; edx = 1 - the lenght of the text to be read
+    	int 0x80		; sys int - reading a char and putting it in x_buffer
     	
-    	mov eax, 0x30 ;
+    	mov eax, 0x30 		; eax = 48
     	
-    	cmp eax, [x_buffer]     ;   '0' = eax <= x_buffer
-    	jle verif_mai_mic
-    	jmp et_citire_y
+	;verifies if the read character is a digit
+    	cmp eax, [x_buffer]     ; '0' = eax <= x_buffer
+    	jle verif_mai_mic	; jump if less or equal
+    	jmp et_citire_y		; skipping the rest of the reading
     	
-    	verif_mai_mic:
-    	    mov eax, 0x39
-    	    cmp eax, [x_buffer]
-    	    jge et_construire_numar
+    	verif_mai_mic:3
+    	    mov eax, 0x39	; eax = ascii code of '9'
+    	    cmp eax, [x_buffer]	; '9' = eax >= x_buffer
+    	    jge et_construire_numar ; '0' <= x_buffer <= '9'
     	
     	jmp et_citire_y
     	
     	et_construire_numar:
 
-            mov ecx, [p]    	
-    	    xor edx, edx
-    	    xor eax, eax
+            mov ecx, [p]    	; ecx = p = 10
+    	    xor edx, edx	; edx = 0
+    	    xor eax, eax	; eax = 0
     	   
     	    mov eax, [x_buffer] ;   eax = 'n' - caracterul citit
-    	    sub eax, 0x30        ;   eax contine o cifra [0,9]
+    	    sub eax, 0x30       ;   eax contine o cifra [0,9]
     	    mov ebx, eax        ;   ebx = cifra
     	    mov eax, [x]        ;   eax = x; x = numarul citit
     	    mul ecx             ;   ecx = p; eax * ecx = x * p
@@ -115,11 +117,11 @@ _start:
     continue:
     
     mov eax, [x]
-    add [y], eax
+    add [y], eax		; y = y + x
     
     
-    xor eax, eax
-    mov [x], eax                ;   x = 0
+    xor eax, eax		; eax = 0
+    mov [x], eax                ; x = 0
     
     
     et_oglindit:
